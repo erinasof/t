9b3d5d3833060c4cd7d5847b2c2be44efd9ca172
erinasof <sofochchka94@mail.ru>
2017-04-05 12:01:01 +0700

    com3
    написание теста с пустым файлом побудила меня дописать метод считывания код из файла на случай, если количество символов кода некорректно

--- test3.cpp
+++ test3.cpp
@@ -21,17 +21,20 @@ public:
 	int **field;
 
 	bool readPasswFile(string s) {
+		bool f1 = true;
 		try {
 			ifstream ifs(s);
 			if (ifs.is_open()) {
 				int n;
 				ifs >> n;
+				if (n < 5 || n > 9) f1 = false;
 				int u;
 				for (int i = 0; i < n; i++) {
 					ifs >> u;
 					code.push_back(u);
 				}
-				return true;
+				if (f1) return true;
+				else return false;
 			}
 			else {
 				return false;
@@ -67,6 +70,18 @@ TEST(readPasswFileTest, Test1) {
 	ofs.close();
 
 	ASSERT_TRUE(p.password.readPasswFile(s));
+}
+
+TEST(readPasswFileTest, Test2) {
+	Prog p;
+	string s = "passw1.txt";
+	ofstream ofs(s);
+	if (ofs.is_open()) {
+		ofs << "1000 8 7 6 4";
+	}
+	ofs.close();
+	//на пустой файл реакция
+	ASSERT_FALSE(p.password.readPasswFile(s));
 }
 
 int _tmain(int argc, _TCHAR* argv[]){
