beaa8f9173af64097fdf486d78880b964dd2d293
erinasof <sofochchka94@mail.ru>
2017-04-05 20:06:01 +0700

    com29: остальное
    без тестов

--- test3.cpp
+++ test3.cpp
@@ -6,10 +6,9 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <iomanip>
 #include <locale.h>
 #include <math.h>
-#include<list>
+#include<Windows.h>
 #include <conio.h>
 #include "gtest/gtest.h"
 using namespace std;
@@ -27,10 +26,114 @@ public:
 		for (int i = 0; i<3; i++)
 			field[i] = new int[3];
 		
-		for (int i = 0; i < 3; i++)
+		makeFlush();
+	}
+
+	//отрисовка на экран поля с крестиками-ноликами
+	void printXO(int **a) {
+		cout << endl << endl;
+		for (int i = 0; i < 3; i++) {
+			cout << "			";
 			for (int j = 0; j < 3; j++) {
-				field[i][j] = 0;
+				switch (a[i][j])
+				{
+				case 0: cout << ". "; break;
+				case 1: cout << "X "; break;
+				case 2: cout << "O "; break;
+				}
 			}
+			cout << endl;
+		}
+	}
+
+	void makeFlush() {
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				field[i][j] = 0;
+	}
+
+	//вход в систему. 1 - вошел, 0 - нет
+	bool logIn() {
+		system("cls");
+
+		makeFlush();
+
+		int c = 0;
+		for (int i = 0; i < code.size(); i++) {
+			if (c % 2 == 0) cout << endl << "input number of place: ";
+			else {
+				cout << "the programm's moving";
+				Sleep(350);
+			}
+			printXO(field);
+			int u;
+			if (c % 2) u = code[i];
+			else u = (int)getch() - 48;
+
+			system("cls");
+			if (code[i] != u) {
+				//всё очень плохо
+				cout << "incorrect code. no access. press any key to continue" << endl;
+				_getch();
+				return false;
+			}
+			else {
+				field[getVert(u)][getHoriz(u)] = (c % 2) ? (2) : (1);
+				c++;
+			}
+		}
+
+		cout << "access!";
+		Sleep(500);
+		fflush(stdin);
+
+		return true;
+
+	}
+
+	//создание кода-пароля и вывод результата (успешно или неуспешно)
+	bool makeCode() {
+		int c = 0;
+		code.clear();
+		makeFlush();
+
+		while (notEndCycle()) {
+			printXO(field);//отрисовка
+			cout << "NEW CODE PASSWORD" << endl << "input number of place: ";
+			fflush(stdin);
+			int u = (int)getch() - 48;
+			if (isCorrectPlace(u)) {
+				field[getVert(u)][getHoriz(u)] = (c % 2) ? (2) : (1);
+				code.push_back(u);
+				c++;
+			}
+			system("cls");
+		}
+		if (!isCorrectCode()) {
+			cout << "incorrect code. try again";
+			//чистим
+			code.clear();
+			readPasswFile("passw.txt");
+			makeFlush();
+			_getch();
+			return 0;
+		}
+
+		//запись удачного варианта в passw.txt
+		ofstream ofs("passw.txt");
+		if (ofs.is_open()) {
+			ofs << code.size();
+			ofs << " ";
+			for (int i = 0; i < code.size(); i++) {
+				ofs << code[i];
+				ofs << " ";
+			}
+		}
+
+		ofs.close();
+		cout << "access!) press any key to continue";
+		_getch();
+		return 1;
 	}
 
 	//вертикальная координата из номера
@@ -105,6 +208,23 @@ class MyAcc {
 public:
 	string myPassword;
 	string myAccName;
+
+	MyAcc initAcc() {
+		system("cls");
+		cout << "input name of account: " << endl;
+		cin >> myAccName;
+		system("cls");
+		cout << "input password: " << endl;
+		cin >> myPassword;
+		system("cls");
+		return (*this);
+	}
+
+	MyAcc editAccPassword() {
+		cout << "input new password: ";
+		cin >> myPassword;
+		return (*this);
+	}
 };
 
 class Prog {
@@ -112,12 +232,71 @@ public:
 	vector <MyAcc> accounts;
 	XO password;
 
+	Prog() {
+		if (!readListFile("mylist.txt"))//если файл поврежден
+			accounts.clear();
+	}
+
+	void saveList() {
+		ofstream ofs("mylist.txt");
+		if (ofs.is_open()) {
+			ofs << accounts.size() << " ";
+			for (int i = 0; i<accounts.size(); i++) {
+				ofs << accounts[i].myAccName << " " << accounts[i].myPassword << " ";
+			}
+		}
+		ofs.close();
+	}
+
 	bool printAccounts() {
 		cout << "LIST OF ACCOUNTS" << endl << endl;
 		int y = 0;
 		for (vector<MyAcc>::iterator r = accounts.begin(); r != accounts.end(); r++, y++)
 			cout << y + 1 << ")	" << r->myAccName << "	" << r->myPassword << endl;
 		return bool(y);
+	}
+
+	void addAccount() {
+		MyAcc a;
+		a.initAcc();
+		accounts.push_back(a);
+		cout << "success! press any key to continue";
+		_getch();
+	}
+
+	void deleteAccount() {
+		int n;
+		while (1) {
+			cout << endl << "input number of account to delete: ";
+			cin >> n;
+			if (n<1 || n>accounts.size())
+				cout << "incorrect number. try again" << endl;
+			else break;
+		}
+
+		int y = 0;
+
+		for (vector<MyAcc>::iterator r = accounts.begin(); r != accounts.end(); r++, y++)
+			if (y == n - 1) {
+				accounts.erase(r);
+				break;
+			}
+		cout << "account was deleted";
+		_getch();
+	}
+
+	void editPassword() {
+		int numb;
+		while (1) {
+			cout << endl << "input number of account to edit password: ";
+			cin >> numb;
+			if (numb<1 || numb>accounts.size())
+				cout << "incorrect number. try again" << endl;
+			else break;
+		}
+		accounts[numb - 1].editAccPassword();
+		cout << endl << "success! press any key to continue";
+		_getch();
 	}
 
 	bool readListFile(string s) {
@@ -146,6 +325,98 @@ public:
 		catch (...) {
 			//что-то пошло не так
 			return false;
+		}
+	}
+
+	void doProg() {
+		char q = 1;
+		while (q ) {
+			if (q ) {
+				if (password.readPasswFile("passw.txt"))
+				{
+					//логинимся, пока не залогинимся
+					while (!password.logIn());
+					//залогинились. вот тут менюшка
+					while (q) {
+						system("cls");
+						cout << "			MAIN MENU" << endl;
+						cout << "1-------------change password" << endl;
+						cout << "2-------------see list" << endl;
+						cout << "3-------------edit list" << endl;
+						cout << "esc-----------close program" << endl;
+
+						char menu1 = getch();
+						system("cls");
+						switch (menu1) {
+						case '1': {
+							//изменить пароль
+							password.makeCode();
+							break;
+						}
+						case '2': {
+							//вывод списка
+							if (!printAccounts())
+								cout << endl << "nothing to show!";
+
+							cout << endl << endl << "press any key to return to main menu";
+							_getch();
+							break;
+						}
+						case '3': {
+							cout << "			MENU" << endl;
+							cout << "1-------------add account" << endl;
+							cout << "2-------------edit account" << endl;
+							cout << "3-------------delete account" << endl;
+							cout << "other---------main menu" << endl;
+
+							char menu2 = getch();
+							system("cls");
+
+							switch (menu2) {
+							case '1': {
+								//добавить новый
+								MyAcc acc;
+								addAccount();
+								saveList();
+								break;
+							}
+							case '2': {
+								//изменить один из паролей
+								if (!printAccounts())
+									cout << endl << "nothing to show!";
+								editPassword();
+								saveList();
+								break;
+							}
+							case '3': {
+								if (!printAccounts())
+									cout << endl << "nothing to show!";
+
+								deleteAccount();
+								saveList();
+								break;
+							}
+							default:
+								break;
+							}
+							break;
+						}
+						case 27: {
+							q = 0;
+							break;
+						}
+						default:
+							cout << "incorrect key. try again after pressing any key";
+							_getch();
+							break;
+						}
+					}
+				}
+				else {
+					cout << "you have no password. create a new password" << endl << endl;
+					while (!password.makeCode());
+				}
+			}
 		}
 	}
 };
@@ -418,8 +689,9 @@ TEST(printAccountsTest, Test2) {
 
 int _tmain(int argc, _TCHAR* argv[]){
 	::testing::InitGoogleTest(&argc, argv);
-	RUN_ALL_TESTS();
+	//RUN_ALL_TESTS();
+	Prog p;
+	p.doProg();
 
-	_getch();
 	return 0;
 }
